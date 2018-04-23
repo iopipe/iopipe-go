218696e65e72c2e86affb827f6dd9fd0e3121f27
Kumbirai Tanekha <kumbirai.tanekha@gmail.com>
2018-04-23 11:58:41 +0100

    clean: comments

--- report_aws_s3.go
+++ report_aws_s3.go
@@ -9,12 +9,11 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 )
 
-// UploadFileToS3 will upload a single file to S3, it will require a pre-built aws session
-// and will set file info like content type and encryption on the uploaded file.
 var awsSharedSession *session.Session
 
 func UploadFileToS3(fileName, fileContents string) {
 	var err error
+
 	// Create a single AWS session (we can re use this if we're uploading many files)
 	if awsSharedSession == nil {
 		awsSharedSession, err = session.NewSession()
@@ -23,17 +22,19 @@ func UploadFileToS3(fileName, fileContents string) {
 		}
 	}
 
-	// Config settings: this is where you choose the bucket, fileName, content-type etc.
-	// of the file you're uploading.
 	svc := s3manager.NewUploader(awsSharedSession)
 
 	fmt.Println("Starting AWS upload")
+
+	// Config settings: this is where you choose the bucket, fileName etc.
+	// of the file you're uploading.
 	_, err = svc.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(os.Getenv("S3_BUCKET")),
 		Key:    aws.String(fileName),
 		Body:   bytes.NewReader([]byte(fileContents)),
 		ACL:    aws.String("public-read"),
 	})
+
 	fmt.Println("Finished AWS upload")
 	fmt.Println(
 		fmt.Sprintf("Report available at https://s3.amazonaws.com/%s/%s",
